87ec335e5ac209002a543417be7d43cd49342e90
jwatkinsm <jwatkinsm@gmail.com>
2025-01-11 18:26:55 -0500

    lesson 1

---
+++ Controllers/index.js
@@ -0,0 +1,5 @@
+const getName = (req, res) => {
+    res.send('Joshua Watkins');
+  };
+  
+  module.exports = {getName};
---
+++ Routes/index.js
@@ -0,0 +1,7 @@
+const routes = require('express').Router();
+
+const baseController = require('../Controllers');
+
+routes.get('/', baseController.getName);
+
+module.exports = routes;
