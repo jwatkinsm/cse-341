854dc44281dc39c2047113c1cbe6357f8fa36cbf
jwatkinsm <jwatkinsm@gmail.com>
2025-02-01 20:46:10 -0500

    frontend intergration

--- server.js
+++ server.js
@@ -11,9 +11,17 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
+	);
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS',
+	);
 	next();
 });
-
 app.use('/', require('./Routes'));
 
 mongodb.initDb((err) => {
