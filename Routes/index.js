c5e041e47e0b9b7b9451760ae860cb9ba06d1d80
jwatkinsm <jwatkinsm@gmail.com>
2025-02-01 20:24:53 -0500

    name by default bug

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
-//const baseController = require('../Controllers/index');
-//routes.get('/', baseController.getName);
+const baseController = require('../Controllers/index');
+routes.get('/', baseController.getName);
 routes.use('/', require('./swagger'));
 routes.use('/contacts', require('./contacts'));
 
